36756fa60a3c8ac637cb2354a3cdd0ebba464033
Sam <sam.hide.ettahiri@gmail.com>
2021-07-27 15:57:48 +0200

    Instaling rspec

---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,19 @@
+RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do |example|
+    DatabaseCleaner.strategy = if %i[feature request].include? example.metadata[:type]
+                                 :truncation
+                               else
+                                 :transaction
+                               end
+
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
