13acba3d4a1493126626afbbf97b9ec825c37e1b
Sam <sam.hide.ettahiri@gmail.com>
2021-08-04 20:40:03 +0200

    Preparing vue for Ranking feature

--- app/admin/rankings.rb
+++ app/admin/rankings.rb
@@ -1,3 +1,32 @@
 ActiveAdmin.register Ranking do
   permit_params :name
+
+  sidebar 'Tier List' do
+    attributes_table_for resource.tier_list do
+      row :ss_coef
+      row :s_coef
+      row :a_coef
+      row :b_coef
+      row :c_coef
+    end
+  end
+
+  show do
+    columns do
+      column do
+        panel 'Tournois' do
+          table_for resource.tournaments do
+            column :name
+            column :dated_at
+            column :tier
+            column :match_sync
+          end
+        end
+      end
+      column do
+        panel 'Classement' do
+        end
+      end
+    end
+  end
 end
