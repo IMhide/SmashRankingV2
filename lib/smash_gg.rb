c0449e8cf055655f4fbe80ec159ab29fbf360670
Sam <sam.hide.ettahiri@gmail.com>
2021-08-04 11:21:30 +0200

    SolarGraph + SmashGG Get Match Lib + small AA sidebar fi

--- lib/smash_gg.rb
+++ lib/smash_gg.rb
@@ -2,6 +2,7 @@ require_relative './smash_gg/finders/base_finder'
 require_relative './smash_gg/finders/get_finished_tournament_by_slug'
 require_relative './smash_gg/finders/get_events_by_tournament_id'
 require_relative './smash_gg/finders/get_event_standings'
+require_relative './smash_gg/finders/get_event_matches'
 
 module SmashGg
 end
---
+++ lib/smash_gg/finders/get_event_matches.rb
@@ -0,0 +1,81 @@
+module SmashGg
+  class GetEventMatches < BaseFinder
+    MatchQuery = GraphClient.parse <<-'GRAPHQL'
+    query($event_id: ID!, $page: Int!, $group_phase_id: [ID]) {
+      event(id: $event_id) {
+        sets(page: $page, perPage: 55, filters: {phaseGroupIds: $group_phase_id}) {
+          nodes {
+            round
+            winnerId
+            completedAt
+            slots {
+              entrant {
+                id
+                participants {
+                  verified
+                  player {
+                    id
+                    prefix
+                    gamerTag
+                  }
+                }
+              }
+              standing{
+                stats{
+                  score{
+                    value
+                  }
+                }
+              }
+            }
+          }
+          pageInfo{
+            totalPages
+          }
+        }
+      }
+    }
+    GRAPHQL
+
+    PhaseGroupQuery = GraphClient.parse <<-'GRAPHQL'
+    query($event_id: ID!) {
+      event(id: $event_id) {
+        phaseGroups{
+          bracketType
+          id
+        }
+      }
+    }
+    GRAPHQL
+
+    def initialize(event_remote_id:)
+      @event_remote_id = event_remote_id
+    end
+
+    def call
+      Array.new(total_page) { |i| matches_query(page: i + 1) }.flatten
+    end
+
+    def total_page
+      GraphClient.query(MatchQuery, variables: { event_id: @event_remote_id, page: 1, group_phase_id: de_phases_ids }).original_hash.dig(
+        'data', 'event', 'sets', 'pageInfo', 'totalPages'
+      )
+    end
+
+    def matches_query(page: 1)
+      results = GraphClient.query(MatchQuery, variables: { event_id: @event_remote_id, page: page, group_phase_id: de_phases_ids }).original_hash.dig(
+        'data', 'event', 'sets', 'nodes'
+      )
+    end
+
+    def de_phases_ids
+      @result ||= GraphClient.query(PhaseGroupQuery, variables: { event_id: @event_remote_id }).original_hash.dig(
+        'data', 'event', 'phaseGroups'
+      ).select do |b|
+                    b['bracketType'] == 'DOUBLE_ELIMINATION'
+                  end.map do |c|
+        c['id']
+      end
+    end
+  end
+end
