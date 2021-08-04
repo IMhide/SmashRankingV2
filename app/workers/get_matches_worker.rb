1111f59f30de20a80077dda8444c1e6f04d5e2d2
Sam <sam.hide.ettahiri@gmail.com>
2021-08-04 13:51:37 +0200

    Finishig first try of GetMatchesWorker

--- app/workers/get_matches_worker.rb
+++ app/workers/get_matches_worker.rb
@@ -2,11 +2,11 @@ class GetMatchesWorker
   include Sidekiq::Worker
 
   def perform(tournament_id, remote_event_id)
+    @tournament = Tournament.find(tournament_id)
     ActiveRecord::Base.transaction do
-      tournament = Tournament.find(tournament_id)
-      actions(tournament: tournament, remote_event_id: remote_event_id)
-      tournament.update(match_sync: :success)
+      actions(remote_event_id: remote_event_id)
     end
+    tournament.update(match_sync: :success)
   rescue Exception => e
     tournament.update(match_sync: :failure)
     raise e
@@ -14,17 +14,32 @@ class GetMatchesWorker
 
   private
 
-  def actions(tournament:, remote_event_id:)
+  def actions(remote_event_id:)
     remote_matches = SmashGg::GetEventMatches.new(event_remote_id: remote_event_id).call
     remote_matches.each do |remote_match|
-      extract_result(remote_match)
+      Match.create!(extract_result(remote_match))
     end
   end
 
   def extract_result(match)
+    winner_slot = match['slots'].find { |s| s['entrant']['id'] == match['winnerId'] }
+    looser_slot = match['slots'].find { |s| s['entrant']['id'] != match['winnerId'] }
+
     {
-      winner: match['slots'].find { |s| s['entrant']['id'] == match['winnerId'] },
-      looser: match['slots'].find { |s| s['entrant']['id'] == match['winnerId'] }
+      winner_id: Player.find_by(remote_id: slot_player_remote_id(winner_slot)).pluck(:id),
+      looser_id: Player.find_by(remote_id: slot_player_remote_id(looser_slot)).pluck(:id),
+      winner_score: slot_score(winner_slot),
+      looser_score: slot_score(looser_slot),
+      tournament_id: @tournament.id,
+      completed_at: Time.at(match['completedAt'])
     }
+  end
+
+  def slot_player_remote_id(slot)
+    slot['entrant']['participants'].first['player']['id']
+  end
+
+  def slot_score(slot)
+    slot['standing']['stats']['score']['value']
   end
 end
