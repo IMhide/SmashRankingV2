72aa374f606ca3490b596bc91f5ab672821fe2f8
Sam <sam.hide.ettahiri@gmail.com>
2021-08-06 19:05:53 +0200

    adding previous ranking management

--- app/services/compute_rating.rb
+++ app/services/compute_rating.rb
@@ -22,12 +22,12 @@ class ComputeRating < BaseService
     r_winner = set_trueskill_ratings(winner_rating, coef)
     r_looser = set_trueskill_ratings(looser_rating, coef)
     graph = Saulabs::TrueSkill::ScoreBasedBayesianRating.new(r_winner => match.winner_score.to_i,
-                                                             r_looser => match.looser_score.to_i)
+      r_looser => match.looser_score.to_i)
     graph.update_skills
     Rating.create!(ranking_id: @ranking.id, match_id: match.id, player_id: match_winner_id, mean: r_winner.first.mean,
-                   deviation: r_winner.first.deviation)
+      deviation: r_winner.first.deviation)
     Rating.create!(ranking_id: @ranking.id, match_id: match.id, player_id: match_looser_id, mean: r_looser.first.mean,
-                   deviation: r_looser.first.deviation)
+      deviation: r_looser.first.deviation)
   end
 
   def get_coef(tournament)
@@ -35,9 +35,15 @@ class ComputeRating < BaseService
   end
 
   def player_rating(player_id, match_id)
-    Rating.where(player_id: player_id, ranking_id: @ranking.id).order(created_at: :desc).first ||
-      Rating.create!(player_id: player_id, match_id: match_id, ranking_id: @ranking.id, mean: BigDecimal('25'),
-                     deviation: BigDecimal(25 / 3), base: true)
+    rating = Rating.where(player_id: player_id, ranking_id: @ranking.id).order(created_at: :desc).first
+
+    if !@ranking.previous_season_id.nil?
+      params = Rating.where(player_id: player_id, ranking_id: @ranking.previous_season_id).order(created_at: :desc).first.attributes
+      params['ranking_id'] = @ranking.id
+      rating = Rating.create!(params.merge({base: true}))
+    end
+
+    rating || Rating.create!(player_id: player_id, match_id: match_id, ranking_id: @ranking.id, mean: BigDecimal('25'), deviation: BigDecimal(25 / 3), base: true)
   end
 
   def set_trueskill_ratings(rating, coef)
