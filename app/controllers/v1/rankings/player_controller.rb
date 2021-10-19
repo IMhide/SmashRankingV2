ae075b4e6ca996931ab08ea904471a3896c0f76d
Sam Ettahiri - Hoggo <sami@hoggo.com>
2021-10-19 20:36:33 +0200

    Updating API Payload + foreigner fix

--- app/controllers/v1/rankings/player_controller.rb
+++ app/controllers/v1/rankings/player_controller.rb
@@ -9,16 +9,27 @@ module V1
           ranking: {
             name: @ranking.name,
             placed_player_count: @ranking.standing.count,
-            all_player_count: @ranking.tmp_standing.count
+            all_player_count: @ranking.tmp_standing.count,
+            standing: @ranking.standing,
+            tmp_standing: @ranking.tmp_standing
           },
-          player: @player,
-          match_count: {
-            won: @player.matches_won.joins(:tournament).where(tournaments: {ranking: @ranking}).count,
-            lost: @player.matches_lost.joins(:tournament).where(tournaments: {ranking: @ranking}).count
+          player_standing: {
+            standing: get_player_standing,
+            match_count: {
+              won: @player.matches_won.joins(:tournament).where(tournaments: {ranking: @ranking}).count,
+              lost: @player.matches_lost.joins(:tournament).where(tournaments: {ranking: @ranking}).count
+            }
           }
         }
 
         render json: ret
+      end
+
+      private
+
+      def get_player_standing
+        @ranking.standing.find { |player| player['id'] == @player.id } ||
+          @ranking.tmp_standing.find { |player| player['id'] == @player.id }
       end
     end
   end
--- app/services/sync_tournament_event.rb
+++ app/services/sync_tournament_event.rb
@@ -13,11 +13,6 @@ class SyncTournamentEvent < BaseService
 
   private
 
-  def set_tier
-    tier = @tournament.ranking.tier_list.find_tier(@tournament.participations.where(dq: false).count)
-    @tournament.update(tier: tier)
-  end
-
   def sync_standings
     SmashGg::GetEventStandings.new(event_remote_id: @remote_event_id).call.each do |standing|
       Participation.create!(seed: standing[:seed], placement: standing[:placement], player: sync_player(standing),
@@ -25,14 +20,22 @@ class SyncTournamentEvent < BaseService
     end
   end
 
-  def sync_player(standing)
-    Player.find_by(remote_id: standing[:player_id]) ||
-      Player.create!(name: standing[:player_name],
-        team: standing[:player_team], remote_id: standing[:player_id])
+  def set_tier
+    tier = @tournament.ranking.tier_list.find_tier(@tournament.participations.where(dq: false).count)
+    @tournament.update(tier: tier)
   end
 
   def sync_matches
     @tournament.update(match_sync: :running)
     GetMatchesWorker.perform_async(@tournament.id, @remote_event_id)
+  end
+
+  def sync_player(standing)
+    db_player = Player.find_by(remote_id: standing[:player_id]) ||
+      Player.create!(name: standing[:player_name],
+        team: standing[:player_team],
+        remote_id: standing[:player_id])
+    db_player.update(team: standing[:player_team]) if db_player.team != standing[:player_team]
+    db_player
   end
 end
