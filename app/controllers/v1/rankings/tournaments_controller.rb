da122bfa2393c8bf5382252cca06392ac203240e
Sam Ettahiri - Hoggo <sami@hoggo.com>
2021-10-23 10:42:47 +0200

    adding match count per tournament

--- app/controllers/v1/rankings/tournaments_controller.rb
+++ app/controllers/v1/rankings/tournaments_controller.rb
@@ -4,8 +4,21 @@ module V1
       def index
         player = Player.find(params[:player_id])
         payload = player.tournaments.includes(:participations)
-          .where(ranking_id: params[:ranking_id]).order(dated_at: :desc)
-        render json: payload, includes: :participations
+          .where(ranking_id: params[:ranking_id]).order(dated_at: :desc).map do |tournament|
+            player_participation = tournament.participations.where(player: player).first
+            all_matches = tournament.matches.where(winner: player).or(tournament.matches.where(looser: player))
+            matches_won = all_matches.count { |m| m.winner_id == player.id }
+            matches_lost = all_matches.count { |m| m.looser_id == player.id }
+
+            {
+              name: tournament.name,
+              tier: tournament.tier,
+              seed: player_participation.seed,
+              placement: player_participation.placement,
+              match_count: "#{matches_won} - #{matches_lost}"
+            }
+          end
+        render json: payload, include: :participations
       end
     end
   end
