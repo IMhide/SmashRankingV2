47d6dcdf2e642687416da1bc712368a96b7feb5b
Sam Ettahiri - Hoggo <sami@hoggo.com>
2021-10-29 21:24:44 +0200

    Plz autoloading god accept second present

--- lib/smash_gg/finders.rb
+++ lib/smash_gg/finders.rb
@@ -1,9 +1,9 @@
-require_relative './smash_gg/finders/base_finder'
-require_relative './smash_gg/finders/get_finished_tournament_by_slug'
-require_relative './smash_gg/finders/get_events_by_tournament_id'
-require_relative './smash_gg/finders/get_event_standings'
-require_relative './smash_gg/finders/get_event_matches'
-require_relative './smash_gg/finders/get_tournament_by_event_id'
+require_relative './finders/base_finder'
+require_relative './finders/get_finished_tournament_by_slug'
+require_relative './finders/get_events_by_tournament_id'
+require_relative './finders/get_event_standings'
+require_relative './finders/get_event_matches'
+require_relative './finders/get_tournament_by_event_id'
 
 module SmashGg
   module Finders
