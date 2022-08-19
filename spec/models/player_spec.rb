ebe12b0c9593d897603fc5dd707de5cc13a2caee
Sam <sam.hide.ettahiri@gmail.com>
2022-08-19 18:22:40 +0200

    Adding test on Player#display_name

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,5 +1,9 @@
-require 'rails_helper'
-
 RSpec.describe Player, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#display_name' do
+    subject { FactoryBot.build(:player, name: name, team: team).display_name }
+    let(:name) { 'Oryon' }
+    let(:team) { 'OPL' }
+
+    it {expect(subject).to eq("#{team} | #{name}")}
+  end
 end
