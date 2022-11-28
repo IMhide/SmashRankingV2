a552c693c30d583faab186a5c64c2ba7799ba0b5
Sam Ettahiri <sam@baijobu.com>
2022-11-28 21:29:37 +0800

    Beggining of a view

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,4 +1,7 @@
 class RankingController < ApplicationController
+  include Pagy::Backend
   def index
+    @ranking = Ranking.last.standing
+    @ranking = @ranking.select { |rank| rank['name'].downcase.include?(params[:query].downcase) } if params[:query]
   end
 end
