5977138878f3d75d993f4cb7110830472d910667
Sam Ettahiri <sam@baijobu.com>
2023-04-24 14:21:28 +0200

    Placement component ok

--- app/controllers/players/rankings_controller.rb
+++ app/controllers/players/rankings_controller.rb
@@ -4,6 +4,9 @@ module Players
       @ranking = Ranking.last
       @query = params[:query]
       @page = params[:page]
+
+      ### Pour les placement mettre la moyenne des placements
+      ### et tu fais moyenne.floor pour avoir un int et pas un float
     end
 
     def show
@@ -12,7 +15,8 @@ module Players
       @match_win = Match.where(winner_id: @player.id, tournament_id: @ranking.tournament_ids)
       @match_lost = Match.where(looser_id: @player.id, tournament_id: @ranking.tournament_ids)
 
-      @placement = "#{@ranking.placement_for(player_id: @player.id)&.position} / #{@ranking.participant_count}"
+      @placement = @ranking.placement_for(player_id: @player.id)
+
       @match_win_count = @match_win.count
       @match_lost_count = @match_lost.count
       @set_win = @match_win.reduce(0) { |r, m| r + m.winner_score }
